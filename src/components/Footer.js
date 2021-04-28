5a4796def749d2951c74ecd835b3782b38a4df7c
JCFavaretto <jeycepoint@gmail.com>
2021-04-27 22:23:59 -0300

    new footer layout

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,12 +9,19 @@ const InnerFooter = styled.section`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  background-color: var(--dark);
   padding: 1rem;
   color: var(--light);
+  background-color: rgba(0, 0, 0, 0.4);
 
   .footer__sociales {
+    display: flex;
+    justify-content: center;
+    align-items: center;
     text-align: center;
+
+    @media (max-width: 700px) {
+      flex-direction: column;
+    }
   }
 
   .footer__iconos {
@@ -22,12 +29,14 @@ const InnerFooter = styled.section`
     justify-content: space-around;
     padding-bottom: 1rem;
     width: 10rem;
-    margin: 0 auto;
+    margin: 0 0 0 1rem;
   }
 
   .footer__copy {
     text-align: center;
     font-size: 0.5rem;
+    padding: 0;
+    margin-top: 1rem;
   }
 `;
 
@@ -35,39 +44,43 @@ function Footer() {
   return (
     <InnerFooter>
       <div>
-        <p className="footer__sociales">Nuestras Redes Sociales</p>
-        <div className="footer__iconos">
-          <a
-            href="https://www.facebook.com/DeDiezIntegracionDeportiva"
-            rel="noreferrer"
-            target="_blank"
-            style={{
-              color: "var(--light)",
-            }}
-          >
-            <img src={facebook} width="35px" alt="F" />
-          </a>
-          <a
-            href="https://www.instagram.com/dediez.id"
-            rel="noreferrer"
-            target="_blank"
-            style={{
-              color: "var(--light)",
-            }}
-          >
-            <img src={instagram} width="35px" alt="F" />
-          </a>
-          <a
-            href="https://api.whatsapp.com/send?phone=5491132970849&text=Buenas! Quisiera conocer mas sobre los servicios que ofreces"
-            rel="noreferrer"
-            target="_blank"
-            style={{
-              padding: "0",
-              margin: "0",
-            }}
-          >
-            <img src={whatsapp} width="35px" alt="F" />
-          </a>
+        <div className="footer__sociales">
+          <p>
+            Seguinos o contactate por cualquiera de nuestras redes sociales!
+          </p>
+          <div className="footer__iconos">
+            <a
+              href="https://www.facebook.com/DeDiezIntegracionDeportiva"
+              rel="noreferrer"
+              target="_blank"
+              style={{
+                color: "var(--light)",
+              }}
+            >
+              <img src={facebook} width="35px" alt="F" />
+            </a>
+            <a
+              href="https://www.instagram.com/dediez.id"
+              rel="noreferrer"
+              target="_blank"
+              style={{
+                color: "var(--light)",
+              }}
+            >
+              <img src={instagram} width="35px" alt="F" />
+            </a>
+            <a
+              href="https://api.whatsapp.com/send?phone=5491132970849&text=Buenas! Quisiera conocer mas sobre los servicios que ofreces"
+              rel="noreferrer"
+              target="_blank"
+              style={{
+                padding: "0",
+                margin: "0",
+              }}
+            >
+              <img src={whatsapp} width="35px" alt="F" />
+            </a>
+          </div>
         </div>
         <p className="footer__copy">
           D10 Asociación Civil- {new Date().getFullYear()}. Todos los derechos
